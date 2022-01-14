78ba49016d6baf68353b66b3a101241df76cecdb
ahmettsdvn <ahmettsdvn@gmail.com>
2022-01-14 17:55:40 +0300

    vuejs-video-site-template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>   <div id="app" class="container-fluid">
+  
+    <!-- TopBar is a menu on top -->
+    <div class="topbar">
+        <div class="topbar-content">
+          <router-link :to="{path: '/'}" class="menu-link">
+            <Link icon="fas fa-home" url="/" text="Home" title="Home" :linkStyle="linkStyle"/>
+          </router-link>
+          <router-link :to="{path: '/favorites/1'}" class="menu-link">
+            <Link icon="fas fa-star" url="/favorites" text="Favorites" title="Favorites" :linkStyle="linkStyle"/>
+          </router-link>
+        </div>
+    </div>   
+    <router-view/>   
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Link component import
+import Link from './components/Link/Link.component.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Link
+  },
+  data() {
+    return{
+      linkStyle: {
+        color: '#606060',
+        border: '1px solid rgba(0,0,0,.36)',
+        display: 'inline',
+        padding: '12px 14px',
+        borderRadius: '10px',
+                   
+      }
+    }
+  }               
 }
 </script>
 
@@ -23,6 +47,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+}.topbar{
+        border-bottom: 1px solid rgba(0,0,0,0.3);
+        margin-bottom: 25px;
+    }
+
+    .topbar-content{
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding: 20px;
+    }
 </style>
